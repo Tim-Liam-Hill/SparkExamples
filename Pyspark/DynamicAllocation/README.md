bfea793d55b79760fcbbad5405f05c59db550123
Tim <u17112592@tuks.co.za>
2024-01-06 13:55:39 +0200

    Added implementation of first challenge

---
+++ Pyspark/DynamicAllocation/README.md
@@ -0,0 +1,8 @@
+Expected output for test.csv: 
+
+
+Green, S2, 28
+
+Red, S2, 29
+
+Yello, S1, 44
--- Pyspark/DynamicAllocation/main.py
+++ Pyspark/DynamicAllocation/main.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession 
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number, col
 import sys
 
 if __name__=="__main__":
@@ -13,6 +15,13 @@ if __name__=="__main__":
              .getOrCreate())
 
 
+    #We could simply use the 'print' function for logging
+    #but that isn't a good habit to develop.
+    #As such, using the log4j facility spark uses. 
+    log4jLogger = spark.sparkContext._jvm.org.apache.log4j 
+    LOGGER = log4jLogger.LogManager.getLogger(__name__)
+    LOGGER.info("Created logger")
+
     file_path = sys.argv[1]
 
     mnm_df = (spark.read.format("csv")
@@ -20,7 +29,20 @@ if __name__=="__main__":
         .option("inferSchema", "true")
         .load(file_path))
     
-    print(mnm_df.count())
+    #challenge
+    
+    LOGGER.info("Getting the states with greatest count of each color")
+
+    #Determine which states have the highest total count for each respective mnm_color (resulting df should have 1 entry for each color)
+
+    mnm_top_states = mnm_df.groupBy("State","Color").sum("Count").withColumnRenamed("sum(Count)", "Total")
+    win = Window.partitionBy("Color").orderBy(mnm_top_states.Total.desc())
+    mnm_top_states.withColumn("row_number", row_number().over(win)) \
+        .filter(col("row_number") == "1") \
+        .select("Color","State", "Total").orderBy("Color").show()
+
+
+    #Determine for each state which mnm_color has the greatest quantity.
 
 
 
