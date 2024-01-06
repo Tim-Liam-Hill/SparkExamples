8927843f8a9263de148cf644908ae8ede152fbe5
Tim <u17112592@tuks.co.za>
2024-01-06 14:37:04 +0200

    Added more logging

--- Pyspark/DynamicAllocation/main.py
+++ Pyspark/DynamicAllocation/main.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
 from pyspark.sql.functions import row_number, col
 import sys
+import time 
 
 if __name__=="__main__":
 
@@ -29,21 +30,24 @@ if __name__=="__main__":
         .option("inferSchema", "true")
         .load(file_path))
     
+    mnm_state_totals = mnm_df.groupBy("State","Color").sum("Count").withColumnRenamed("sum(Count)", "Total")
+
     #challenge
     
     LOGGER.info("Getting the states with greatest count of each color")
 
     #Determine which states have the highest total count for each respective mnm_color (resulting df should have 1 entry for each color)
 
-    mnm_top_states = mnm_df.groupBy("State","Color").sum("Count").withColumnRenamed("sum(Count)", "Total")
-    win = Window.partitionBy("Color").orderBy(mnm_top_states.Total.desc())
-    mnm_top_states.withColumn("row_number", row_number().over(win)) \
+    win = Window.partitionBy("Color").orderBy(mnm_state_totals.Total.desc())
+    mnm_state_totals.withColumn("row_number", row_number().over(win)) \
         .filter(col("row_number") == "1") \
         .select("Color","State", "Total").orderBy("Color").show()
 
+    LOGGER.info("Getting the color with the greatest count for each state")
 
     #Determine for each state which mnm_color has the greatest quantity.
 
-
-
-
+    win2 = Window.partitionBy("State").orderBy(mnm_state_totals.Total.desc())
+    mnm_state_totals.withColumn("row_number",row_number().over(win2)) \
+        .filter(col("row_number") == "1") \
+        .select("State","Color", "Total").orderBy("State").show()
